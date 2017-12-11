7fc2b903fb970fa5e54e07ac7be15798457490c3
aleyalunin <ale.yalunin@gmail.com>
2017-12-12 01:45:37 +0300

    Smart-contract with nicknames

--- Enstagrm.sol
+++ Enstagrm.sol
@@ -8,10 +8,11 @@ contract Enstagrm {
     * 
     */
     
-    address[] users;
+    bytes16[] users;
     
-    mapping (address => bytes32) addressNicknames;
-    mapping (bytes32 => address) nicknameAddress;
+    mapping (address => bytes16) addressNicknames;
+    mapping (bytes16 => address) nicknameAddress;
+    
     
     modifier onlyRegistredUser()
     {
@@ -20,16 +21,19 @@ contract Enstagrm {
     }
     
     
+    event UserRegistred (address userAddress, bytes32 userNickname);
+    
+    
     function addressIsRegistred(address _address) view public returns(bool) {
-        return addressNicknames[_address] != bytes32(0x0);
+        return addressNicknames[_address] != bytes16(0x0);
     }
     
-    function nicknameIsAvailable(bytes32 _nickname) view public returns(bool) {
+    function nicknameIsAvailable(bytes16 _nickname) view public returns(bool) {
         return nicknameAddress[_nickname] == address(0);
     }
     
     
-    function register(bytes32 _nickname) public {
+    function register(bytes16 _nickname) public {
         require(!addressIsRegistred(msg.sender)); 
         require(_nickname.length > 0 );
         require(nicknameIsAvailable(_nickname)); 
@@ -37,15 +41,17 @@ contract Enstagrm {
         addressNicknames[msg.sender] = _nickname;
         nicknameAddress[_nickname] = msg.sender;
         
-        users.push(msg.sender);
+        users.push(_nickname);
+        
+        UserRegistred(msg.sender, _nickname);
     }
     
-    function getUserNickname(address _address) view public returns(bytes32) {
+    function getUserNickname(address _address) view public returns(bytes16) {
         return addressNicknames[_address];
     }
     
     
-    function getUsers() view public returns(address[]) {
+    function getUsers() view public returns(bytes16[]) {
         return users;
     }
     
@@ -74,6 +80,7 @@ contract Enstagrm {
     // }
     
     
+    
     /**
     * 
     * PROFILE LOGIC
@@ -86,58 +93,66 @@ contract Enstagrm {
         string urlProfileImage;
         
         uint32[] postIds;
-        address[] followers;
-        address[] followings;
+        bytes16[] followers;
+        bytes16[] followings;
     }
 
 
-    mapping (address => Profile) addressProfiles;
+    mapping (bytes16 => Profile) nicknameProfiles;
+    
+    
+    event ProfileUpdated (bytes16 userNickname, bytes32 realName, string bio, string urlProfileImage);
     
     
     function fillProfile(bytes32 _realName, string _bio, string _ipfsLink) public onlyRegistredUser {
-        addressProfiles[msg.sender].realName = _realName;
-        addressProfiles[msg.sender].bio = _bio;
-        addressProfiles[msg.sender].urlProfileImage = _ipfsLink;
+        bytes16 msgSenderNickname = getUserNickname(msg.sender);
+        
+        nicknameProfiles[msgSenderNickname].realName = _realName;
+        nicknameProfiles[msgSenderNickname].bio = _bio;
+        nicknameProfiles[msgSenderNickname].urlProfileImage = _ipfsLink;
+        
+        ProfileUpdated(msgSenderNickname, _realName, _bio, _ipfsLink);
     }
 
-    function getUserProfile(address _address) view public returns(bytes32, string, string) {
+    function getUserProfile(bytes16 _userNickname) view public returns(bytes32, string, string) {
         return (
-            addressProfiles[_address].realName, 
-            addressProfiles[_address].bio, 
-            addressProfiles[_address].urlProfileImage
+            nicknameProfiles[_userNickname].realName, 
+            nicknameProfiles[_userNickname].bio, 
+            nicknameProfiles[_userNickname].urlProfileImage
         );
     }
     
     // Not yet implemented in solidity
     //
     // function getUserPosts(address _address) view public returns(Post[]) {
-    //     return addressProfiles[_address].posts;
+    //     return nicknameProfiles[_address].posts;
     // }
     
-    function getUserPostIds(address _address) view public returns(uint32[]) {
-        return addressProfiles[_address].postIds;
+    function getUserPostIds(bytes16 _userNickname) view public returns(uint32[]) {
+        return nicknameProfiles[_userNickname].postIds;
     }
     
-    function getUserFollowers(address _address) view public returns(address[]) {
-        return addressProfiles[_address].followers;
+    function getUserFollowers(bytes16 _userNickname) view public returns(bytes16[]) {
+        return nicknameProfiles[_userNickname].followers;
     }
     
-    function getUserFollowings(address _address) view public returns(address[]) {
-        return addressProfiles[_address].followings;
+    function getUserFollowings(bytes16 _userNickname) view public returns(bytes16[]) {
+        return nicknameProfiles[_userNickname].followings;
     }
     
     
-    function countUserPosts(address _address) view public returns(uint) {
-        return addressProfiles[_address].postIds.length;
+    function countUserPosts(bytes16 _userNickname) view public returns(uint) {
+        return nicknameProfiles[_userNickname].postIds.length;
     }
     
-    function countUserFollowers(address _address) view public returns(uint) {
-        return addressProfiles[_address].followers.length;
+    function countUserFollowers(bytes16 _userNickname) view public returns(uint) {
+        return nicknameProfiles[_userNickname].followers.length;
     }
     
-    function countUserFollowings(address _address) view public returns(uint) {
-        return addressProfiles[_address].followings.length;
+    function countUserFollowings(bytes16 _userNickname) view public returns(uint) {
+        return nicknameProfiles[_userNickname].followings.length;
     }
+    
     
     
     /**
@@ -146,45 +161,58 @@ contract Enstagrm {
     * 
     */
     
-    mapping (address => mapping (address => bool)) subscriptions;
+    mapping (bytes16 => mapping (bytes16 => bool)) subscriptions;
     
     
-    function follow(address _address) public onlyRegistredUser {
-        require(addressIsRegistred(_address));
-        require(subscriptions[msg.sender][_address] == false);
-        require(_address != msg.sender);
+    event SubscriptionHappened (bytes16 follower, bytes16 following);
+    event UnsubscribingHappened (bytes16 follower, bytes16 unfollowing);
+    
+    
+    function follow(bytes16 _userNickname) public onlyRegistredUser {
+        bytes16 msgSenderNickname = getUserNickname(msg.sender);
         
-        addressProfiles[_address].followers.push(msg.sender);
-        addressProfiles[msg.sender].followings.push(_address);
+        require(!nicknameIsAvailable(_userNickname));
+        require(subscriptions[msgSenderNickname][_userNickname] == false);
+        require(msgSenderNickname != _userNickname);
         
-        subscriptions[msg.sender][_address] = true;
+        nicknameProfiles[_userNickname].followers.push(msgSenderNickname);
+        nicknameProfiles[msgSenderNickname].followings.push(_userNickname);
+        
+        subscriptions[msgSenderNickname][_userNickname] = true;
+        
+        SubscriptionHappened(msgSenderNickname, _userNickname);
     }
 
-    function unfollow(address _address) public onlyRegistredUser {
-        require(addressIsRegistred(_address));
-        require(subscriptions[msg.sender][_address] == true);
-        require(_address != msg.sender);
+    function unfollow(bytes16 _userNickname) public onlyRegistredUser {
+        bytes16 msgSenderNickname = getUserNickname(msg.sender);
         
-        for (uint i = 0; i < countUserFollowers(_address); i++) {
-            if (addressProfiles[_address].followers[i] == msg.sender) {
-                delete addressProfiles[_address].followers[i];
-                var insexOfLastFollower = countUserFollowers(_address) - 1;
-                addressProfiles[_address].followers[i] = addressProfiles[_address].followers[insexOfLastFollower];
-                addressProfiles[_address].followers.length--;
+        require(!nicknameIsAvailable(_userNickname));
+        require(subscriptions[msgSenderNickname][_userNickname] == true);
+        require(msgSenderNickname != _userNickname);
+        
+        for (uint i = 0; i < countUserFollowers(_userNickname); i++) {
+            if (nicknameProfiles[_userNickname].followers[i] == msgSenderNickname) {
+                delete nicknameProfiles[_userNickname].followers[i];
+                var insexOfLastFollower = countUserFollowers(_userNickname) - 1;
+                nicknameProfiles[_userNickname].followers[i] = nicknameProfiles[_userNickname].followers[insexOfLastFollower];
+                nicknameProfiles[_userNickname].followers.length--;
             }
         }
         
-        for (uint j = 0; j < countUserFollowings(msg.sender); j++) {
-            if (addressProfiles[msg.sender].followings[j] == _address) {
-                delete addressProfiles[msg.sender].followings[j];
-                var insexOfLastFollowing = countUserFollowings(msg.sender) - 1;
-                addressProfiles[msg.sender].followings[j] = addressProfiles[msg.sender].followings[insexOfLastFollowing];
-                addressProfiles[msg.sender].followings.length--;
+        for (uint j = 0; j < countUserFollowings(msgSenderNickname); j++) {
+            if (nicknameProfiles[msgSenderNickname].followings[j] == _userNickname) {
+                delete nicknameProfiles[msgSenderNickname].followings[j];
+                var insexOfLastFollowing = countUserFollowings(msgSenderNickname) - 1;
+                nicknameProfiles[msgSenderNickname].followings[j] = nicknameProfiles[msgSenderNickname].followings[insexOfLastFollowing];
+                nicknameProfiles[msgSenderNickname].followings.length--;
             }
         }
         
-        subscriptions[msg.sender][_address] = false;
+        subscriptions[msgSenderNickname][_userNickname] = false;
+        
+        UnsubscribingHappened(msgSenderNickname, _userNickname);
     }
+
 
 
     /**
@@ -192,48 +220,51 @@ contract Enstagrm {
     * POST ADDING LOGIC
     * 
     */
-    event NewPostCreated(address creator, uint32 postId, uint64 timestamp);
-    
-    Post[] posts;
     
     struct Post {
-        address creator;
+        bytes16 creator;
         string urlPhoto;
         string text;
         uint64 timestamp;
         
-        address[] likes;
+        bytes16[] likes;
         uint32[] comments; // unique comment ids
     }
     
-
+    Post[] posts;
+    
+    
+    event PostAdded(uint32 postId, bytes16 creator, string urlPhoto, string text, uint64 timestamp);
+    
+    
     function addPost(string _urlPhoto, string _text) public onlyRegistredUser returns (uint32) {
         bytes memory b1 = bytes(_urlPhoto);
         bytes memory b2 = bytes(_text);
         require(b1.length > 0 && b2.length > 0);
         
+        bytes16 msgSenderNickname = getUserNickname(msg.sender);
+        
         Post memory newPost = Post({
-            creator: msg.sender,
+            creator: msgSenderNickname,
             urlPhoto: _urlPhoto,
             text: _text,
             timestamp: uint64(now),
-            likes: new address[](0),
+            likes: new bytes16[](0),
             comments: new uint32[](0)
         });
         uint32 newPostId = uint32(posts.push(newPost)) - 1;
         
-        addressProfiles[msg.sender].postIds.push(newPostId);
+        nicknameProfiles[msgSenderNickname].postIds.push(newPostId);
         
-        NewPostCreated(msg.sender, newPostId, uint64(now));
+        PostAdded(newPostId, msgSenderNickname, _urlPhoto, _text, uint64(now));
         
         return newPostId;
     }
     
-    function getPost(uint32 _id) view public returns(address, string, string, uint64) {
+    function getPost(uint32 _id) view public returns(bytes16, string, string, uint64) {
         Post storage post = posts[_id];
         return (post.creator, post.urlPhoto, post.text, post.timestamp);
     }
-
 
 
     /**
@@ -242,16 +273,23 @@ contract Enstagrm {
     * 
     */
 
-    mapping (address => mapping (uint32 => bool)) userPostLikes;
+    mapping (bytes16 => mapping (uint32 => bool)) userPostLikes;
+    
+    
+    event PostLiked(uint32 postId, bytes16 creator, uint64 timestamp);
     
     
     function like(uint32 _id) public onlyRegistredUser {
-        require(userPostLikes[msg.sender][_id] == false);
-        posts[_id].likes.push(msg.sender);
-        userPostLikes[msg.sender][_id] = true;
+        bytes16 msgSenderNickname = getUserNickname(msg.sender);
+        require(userPostLikes[msgSenderNickname][_id] == false);
+        
+        posts[_id].likes.push(msgSenderNickname);
+        userPostLikes[msgSenderNickname][_id] = true;
+        
+        PostLiked(_id, msgSenderNickname, uint64(now));
     }
     
-    function getPostLikes(uint32 _id) view public returns(address[]) {
+    function getPostLikes(uint32 _id) view public returns(bytes16[]) {
         return posts[_id].likes;
     }
     
@@ -266,22 +304,30 @@ contract Enstagrm {
     * 
     */
     
-    mapping (uint32 => Comment[]) postComments;
-    
     struct Comment {
-        address creator;
+        bytes16 creator;
         string text;
         uint64 timestamp;
     }
     
+    mapping (uint32 => Comment[]) postComments;
+    
+    
+    event PostCommented(uint32 postId, bytes16 creator, string text, uint64 timestamp);
+    
+    
     function comment(uint32 _id, string _text) public onlyRegistredUser {
         require(bytes(_text).length != 0);
         
+        bytes16 msgSenderNickname = getUserNickname(msg.sender);
+        
         postComments[_id].push(Comment({
-            creator: msg.sender,
+            creator: msgSenderNickname,
             text: _text,
             timestamp: uint64(now)
         }));
+        
+        PostCommented(_id, msgSenderNickname, _text, uint64(now));
     }
     
     
@@ -291,7 +337,7 @@ contract Enstagrm {
     //     return postComments[_id];
     // }
     
-    function getPostComment(uint32 _postId, uint index) view public returns(address, string, uint64) {
+    function getPostComment(uint32 _postId, uint index) view public returns(bytes16, string, uint64) {
         Comment storage comment = postComments[_postId][index];
         return (comment.creator, comment.text, comment.timestamp);
     }
